318234888ce22ce95b71bc26ca28ede3e97c9ef7
Mehdi Oulad Khlie <mehdi.ouladkhlie@icloud.com>
2025-01-16 21:24:10 +0100

    Enhance styling and layout for contact form; improve responsiveness and error message visibility

--- contact/contactform_secure.php
+++ contact/contactform_secure.php
@@ -1,10 +1,10 @@
 <?php
 
-// Show all errors (for educational purposes)
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 
-// Constanten (connectie-instellingen databank)
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'yutaka');
 define('DB_PASS', 'Aza@ever7');
@@ -12,7 +12,7 @@ define('DB_NAME', 'labo05_db');
 
 date_default_timezone_set('Europe/Brussels');
 
-// Verbinding maken met de databank
+
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -21,25 +21,25 @@ try {
     exit;
 }
 
-$name = isset($_POST['name']) ? (string)$_POST['name'] : '';
-$email = isset($_POST['email']) ? (string)$_POST['email'] : '';
-$message = isset($_POST['message']) ? (string)$_POST['message'] : '';
+$name = isset($_POST['name']) ? (string) $_POST['name'] : '';
+$email = isset($_POST['email']) ? (string) $_POST['email'] : '';
+$message = isset($_POST['message']) ? (string) $_POST['message'] : '';
 $msgName = '';
 $msgEmail = '';
 $msgMessage = '';
 
-// form is sent: perform formchecking!
+
 if (isset($_POST['btnSubmit'])) {
 
     $allOk = true;
 
-    // name not empty
+
     if (trim($name) === '') {
         $msgName = 'Gelieve een naam in te voeren';
         $allOk = false;
     }
 
-    // email not empty and valid
+
     if (trim($email) === '') {
         $msgEmail = 'Gelieve een e-mailadres in te voeren';
         $allOk = false;
@@ -48,19 +48,19 @@ if (isset($_POST['btnSubmit'])) {
         $allOk = false;
     }
 
-    // message not empty
+
     if (trim($message) === '') {
         $msgMessage = 'Gelieve een boodschap in te voeren';
         $allOk = false;
     }
 
-    // end of form check. If $allOk still is true, then the form was sent in correctly
+
     if ($allOk) {
-        // build & execute prepared statement
+
         $stmt = $pdo->prepare('INSERT INTO messages (sender, email, message, added_on) VALUES (?, ?, ?, ?)');
         $stmt->execute(array($name, $email, $message, (new DateTime())->format('Y-m-d H:i:s')));
 
-        // the query succeeded, redirect to the thank you page
+
         if ($pdo->lastInsertId() !== 0) {
             header('Location: formchecking_thanks.php?name=' . urlencode($name));
             exit();
@@ -73,41 +73,45 @@ if (isset($_POST['btnSubmit'])) {
 
 ?><!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <title>Testform</title>
-    <meta charset="UTF-8"/>
-    <link rel="stylesheet" type="text/css" href="./styles.css"/>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="./styles.css" />
 </head>
+
 <body>
-<main class="container">
-        <a href="javascript:history.back()" class="back-button">← Terug</a>
-</main>
+    <a href="javascript:history.back()" class="back-button">← Terug</a>
 
-<main class="container">
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <h1>Testform</h1>
-        <p class="message">Alle velden zijn verplicht, tenzij anders aangegeven.</p>
+    <main class="container">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <h1>Testform</h1>
+            <p class="message">Alle velden zijn verplicht, tenzij anders aangegeven.</p>
 
-        <div>
-            <label for="name">Jouw naam</label>
-            <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name); ?>" class="input-text" required/>
-            <span class="message error"><?php echo $msgName; ?></span>
-        </div>
+            <div>
+                <label for="name">Jouw naam</label>
+                <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name); ?>"
+                    class="input-text" required />
+                <span class="message error"><?php echo $msgName; ?></span>
+            </div>
 
-        <div>
-            <label for="email">Jouw e-mail</label>
-            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($email); ?>" class="input-text" required/>
-            <span class="message error"><?php echo $msgEmail; ?></span>
-        </div>
+            <div>
+                <label for="email">Jouw e-mail</label>
+                <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($email); ?>"
+                    class="input-text" required />
+                <span class="message error"><?php echo $msgEmail; ?></span>
+            </div>
 
-        <div>
-            <label for="message">Boodschap</label>
-            <textarea name="message" id="message" rows="5" cols="40" required><?php echo htmlspecialchars($message); ?></textarea>
-            <span class="message error"><?php echo $msgMessage; ?></span>
-        </div>
+            <div>
+                <label for="message">Boodschap</label>
+                <textarea name="message" id="message" rows="5" cols="40"
+                    required><?php echo htmlspecialchars($message); ?></textarea>
+                <span class="message error"><?php echo $msgMessage; ?></span>
+            </div>
 
-        <input type="submit" id="btnSubmit" name="btnSubmit" value="Verstuur"/>
-    </form>
-</main>
+            <input type="submit" id="btnSubmit" name="btnSubmit" value="Verstuur" />
+        </form>
+    </main>
 </body>
+
 </html>
